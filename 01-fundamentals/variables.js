f0ec2b5113426e624d7b166640d1117cb2f6e181
Timbaines <timbaines@timbaines.com>
2025-10-03 17:23:35 -0400

    add: Day 1 - variables.js file with let, var, and const examples.

---
+++ 01-fundamentals/variables.js
@@ -0,0 +1,25 @@
+// Variables are values in your code that can represent different values each time the code runs.
+
+firstname = "Tim";
+x = 2;
+
+// They can be assigned different values each time the code runs.
+
+firstname = "Timothy";
+x = 3;
+
+// Declaring Variables
+
+let firstname = "Timothy";
+firstname = "Tim";
+
+// let, var, and const are all different ways to declare variables.
+
+let tb1 = 4;
+let tb2 = 5;
+const PI = 3.14159;
+
+// const is a way to declare a variable that can't be changed.
+
+const exampleConstant = 3;
+exampleConstant = 4; // output: Uncaught TypeError: Assignment to constant variable.
