b985f0c101cbd03ef0c2c7133abfa24591729e5d
Timbaines <timbaines@timbaines.com>
2025-10-17 17:43:52 -0400

    add: Day 2 - 01-Fundamentals folder - operators.js - practice exercises with arithmetic operators (addition and subtraction) and string concatenation.

---
+++ 01-fundamentals/operators.js
@@ -0,0 +1,33 @@
+// Operators come into play when you want to work with variables, modify them, perform calculations and compare them.
+
+// Arithmetic Operators
+
+let tb1 = 20;
+let tb2 = 4;
+
+let result1 = tb1 + tb2;
+console.log(result1); // Output: 24
+
+// String Concatenation
+
+let str1 = "What up ";
+let str2 = "addition";
+
+let result2 = str1 + str2;
+console.log(result2); // Output: What up addition
+
+// Practice with Operators
+
+let name = "Tim";
+let age = 44;
+
+let result3 = "My name is " + name + ", I am " + age + " years old and learning to code JavaScript";
+console.log(result3); // Output: My name is Tim, I am 44 years old and learning to code JavaScript
+
+// Subtraction Operators
+
+let tb3 = 44;
+let tb4 = 23;
+
+let result4 = tb3 - tb4;
+console.log(result4); // Output: 21
