f62e2972bf6490ea7ffbe2b2adc163e9c23b0926
Timbaines <timbaines@timbaines.com>
2025-10-20 10:31:26 -0400

    add: Day 3 - 01-Fundamentals folder - operators.js - practice exercises with arithmetic operators (multiplication, division, exponentiation and modulus).

--- 01-fundamentals/operators.js
+++ 01-fundamentals/operators.js
@@ -16,13 +16,19 @@ let str2 = "addition";
 let result2 = str1 + str2;
 console.log(result2); // Output: What up addition
 
-// Practice with Operators
+// Practice with Operators (age, name, true/false variables)
 
 let name = "Tim";
 let age = 44;
 
 let result3 = "My name is " + name + ", I am " + age + " years old and learning to code JavaScript";
+
 console.log(result3); // Output: My name is Tim, I am 44 years old and learning to code JavaScript
+
+let isStudent = true;
+let result3WithBool = result3 + " = Student: " + isStudent;
+
+console.log(result3WithBool); // Output: My name is Tim, I am 44 years old and learning to code JavaScript = Student: true
 
 // Subtraction Operators
 
@@ -31,3 +37,61 @@ let tb4 = 23;
 
 let result4 = tb3 - tb4;
 console.log(result4); // Output: 21
+
+let tb5 = 44;
+let tb6 = 8;
+let str3 = "Hello";
+let tb7 = 10
+let result5 = tb5 - tb6;
+let result6 = str3 - tb7;
+
+console.log(result5, result6); // Output: 36 NaN
+
+// Multiplication Operators
+
+let tb8 = 44;
+let tb9 = 14;
+let str4 = "Hello";
+let tb10 = 7;
+let result7 = tb8 * tb9;
+let result8 = str4 * tb10;
+
+console.log(result7, result8); // Output 616 NaN
+
+// Division Operators
+
+let tb11 = 44;
+let tb12 = 11;
+let result9 = tb11 / tb12;
+
+console.log(result9); // Output: 4
+
+// Exponential Operators
+
+let sb1 = 44;
+let sb2 = 3;
+let result10 = sb1 ** sb2;
+
+console.log(result10); // Output: 85184
+
+// Modulus Operator
+// example 1:
+let sb3 = 11;
+let sb4 = 4;
+let result11 = sb3 % sb4;
+
+console.log(`${sb3} % ${sb4} = ${result11}`); // Output: 11 % 4 = 3
+// example 2:
+let sb5 = 9;
+let sb6 = 3;
+let result12 = sb5 % sb6;
+
+console.log(`${sb5} % ${sb6} = ${result12}`); // Output: 9 % 3 = 0
+// example 3:
+let sb7 = 21;
+let sb8 = 6;
+let result13 = sb7 % sb8;
+
+console.log(`${sb7} % ${sb8} = ${result13}`); // Output: 21 % 6 = 3
+
+
