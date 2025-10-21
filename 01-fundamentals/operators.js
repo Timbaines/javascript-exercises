d2daa4fb4381b023587d6edfd156a04493940899
Timbaines <timbaines@timbaines.com>
2025-10-21 09:38:41 -0400

    add: Day 4 - 01-Fundamentals folder - operators.js - practice exercises with arithmetic operators (Unary operators, prefix increment, prefix decrement, postfix increment, postfix decrement).

--- 01-fundamentals/operators.js
+++ 01-fundamentals/operators.js
@@ -16,7 +16,7 @@ let str2 = "addition";
 let result2 = str1 + str2;
 console.log(result2); // Output: What up addition
 
-// Practice with Operators (age, name, true/false variables)
+// Practice with Operators and string concatenation (age, name)
 
 let name = "Tim";
 let age = 44;
@@ -25,6 +25,7 @@ let result3 = "My name is " + name + ", I am " + age + " years old and learning 
 
 console.log(result3); // Output: My name is Tim, I am 44 years old and learning to code JavaScript
 
+// Practice with Operators and string concatenation (age, name, true/false variables)
 let isStudent = true;
 let result3WithBool = result3 + " = Student: " + isStudent;
 
@@ -93,5 +94,41 @@ let sb8 = 6;
 let result13 = sb7 % sb8;
 
 console.log(`${sb7} % ${sb8} = ${result13}`); // Output: 21 % 6 = 3
+
+// Unary Operators (increment and decrement)
+
+// Increment Operator
+let tw1 = 5;
+tw1++;
+
+console.log(tw1); // Output: 6
+
+// Decrement Operator
+let tw2 = 4;
+tw2--;
+
+console.log(tw2); // Output: 3
+
+// Prefix and Postfix Operators
+
+// Postfix Operator
+let tw3 = 2;
+console.log(tw3++); // Output: 2
+console.log(tw3); // Output: 3
+
+// Prefix Operator
+let tw4 = 2;
+console.log(++tw4); // Output: 3
+
+// This example uses the mathematical order of operations to calculate the value of tw5 + tw6 * tw7. For multiplying
+// Uses 6 (prefix, 5 is incremented before multiplication)
+// 2 (postfix, 2 is incremented after the execution and will not affect the current calculation).
+// This comes down to 12 and the tw5 is the postfix operator and will execute after the addition.
+// It adds 12 to 4 and becomes 16.
+let tw5 = 4;
+let tw6 = 5;
+let tw7 = 2;
+
+console.log(tw5++ + ++tw6 * tw7++); // Output: 16
 
 
